2a5d226b285e5da226551a4d8e3baaf8aaeca062
js-0528 <jinschofield@gmail.com>
2025-05-22 01:24:07 -0400

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/research-website-template' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/research-website-template/' : '',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
     remotePatterns: [
